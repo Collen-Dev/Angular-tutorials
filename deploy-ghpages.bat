2640da7fe74b50db5287484b6cb63413517381c1
Malatjie CT <malatjiecollen@gmail.com>
2025-12-05 02:28:31 +0200

    updated the deploy-ghpages with the correct values

--- deploy-ghpages.bat
+++ deploy-ghpages.bat
@@ -3,8 +3,9 @@ REM -----------------------------
 REM Angular GitHub Pages Deploy Script
 REM -----------------------------
 
-REM 1. Set your GitHub repo name (replace with yours)
-SET REPO_NAME=Angular-tutorials
+REM 1. Set your GitHub repo name (replace with yours), but
+REM in my case, this is a project name
+SET REPO_NAME=ctbusiness.ui.app
 
 REM 2. Clean previous build
 IF EXIST dist rmdir /s /q dist
@@ -19,7 +20,7 @@ IF ERRORLEVEL 1 (
 
 REM 4. Deploy to GitHub Pages
 echo Deploying to GitHub Pages...
-npx angular-cli-ghpages --dir="dist/%REPO_NAME%"
+npx angular-cli-ghpages --dir="dist/%REPO_NAME%/browser"
 IF ERRORLEVEL 1 (
     echo Deployment failed!
     exit /b 1
