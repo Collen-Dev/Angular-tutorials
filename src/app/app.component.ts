6d665bdbfb6d5583c58d28c157f36bbdcd686740
Malatjie CT <malatjiecollen@gmail.com>
2025-12-07 15:40:45 +0200

    Added navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
+import {NavBarComponent} from './components/nav-bar/nav-bar.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
